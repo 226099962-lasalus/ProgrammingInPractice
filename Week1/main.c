7c2eecdabf4057d5edc3f0ca919c403efe44505b
Your Name <you@example.com>
2026-08-03 15:49:24 +0200

    Week 1 -Municipal Financial Management System

--- Week1/main.c
+++ Week1/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("Municipal Financial Management System\n"); 
-    printf("Welcome to Windhoek Municipality\n");
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    printf("Municipal Financial Managament System\n\n");
+
+    printf("Enter Municipal Name:");
+    scanf(" %[^\n]s", municipality);
+
+    printf("Enter Mayor:");
+    scanf(" %[^\n]s", mayor);
+
+    printf("Enter Population:");
+    scanf("%d", &population);
+
+    printf("|n--------------------------\n");
+    printf("Municipality : %s\n", municipality);
+    printf("Mayor   :%s\n", mayor);
+    printf("Population  :%d\n",population);
 
     return 0;
 
